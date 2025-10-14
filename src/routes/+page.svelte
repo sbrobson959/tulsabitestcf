8aac7e07d84317f27aa1e95740d60c34ab03d555
Sam <sbrobson@crimson.ua.edu>
2025-10-14 13:05:15 -0500

    Added TCF link

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
-<div class="h-screen flex flex-col">
-	<header class="flex h-15 w-full items-center bg-black px-3 border-zinc-600 border-b">
-		<img src="/tcflogo.png" class="h-10" alt="Terence Crutcher Foundation logo" />
-		<h1 class="text-white pl-3 text-2xl font-title">TULSA ANIMAL BITES</h1>
-		<p class="ml-auto text-gray-400 text-sm">Last Updated: October 13, 2025</p>
+<div class="flex h-screen flex-col">
+	<header class="flex h-15 w-full items-center border-b border-zinc-600 bg-black px-3">
+		<a href="https://terencecrutcherfoundation.org">
+			<img src="/tcflogo.png" class="h-10" alt="Terence Crutcher Foundation logo" />
+		</a>
+		<h1 class="pl-3 font-title text-2xl text-white">TULSA ANIMAL BITES</h1>
+		<p class="ml-auto text-sm text-gray-400">Last Updated: October 13, 2025</p>
 	</header>
 	<iframe class="w-full grow" src="/bites.html" title="Kepler"></iframe>
 </div>
