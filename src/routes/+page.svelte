e9de3945c586ece3ac9387a083bd95c09374a8cb
Sam <sbrobson@crimson.ua.edu>
2025-10-14 14:58:12 -0500

    Fixed header on mobile

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,12 @@
 <div class="flex h-screen flex-col">
-	<header class="flex h-15 w-full items-center border-b border-zinc-600 bg-black px-3">
-		<a href="https://terencecrutcherfoundation.org">
+	<header class="overflow flex h-15 items-center border-b border-zinc-600 bg-black px-3">
+		<a href="https://terencecrutcherfoundation.org" class="shrink-0">
 			<img src="/tcflogo.png" class="h-10" alt="Terence Crutcher Foundation logo" />
 		</a>
-		<h1 class="pl-3 font-title text-2xl text-white">TULSA ANIMAL BITES</h1>
-		<p class="ml-auto text-sm text-gray-400">Last Updated: October 13, 2025</p>
+		<div class="pl-3">
+		<h1 class="font-title text-2xl text-white">TULSA ANIMAL BITES</h1>
+		<p class="-mt-1 pl-px ml-auto text-sm text-gray-400">Last Updated: October 13, 2025</p>
+		</div>
 	</header>
-	<iframe class="w-full grow" src="/bites.html" title="Kepler"></iframe>
+	<iframe class="w-full grow bg-black" src="/bites.html" title="Kepler"></iframe>
 </div>
