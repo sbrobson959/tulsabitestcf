2abff1628e4cd05a0e43f95377acd6af2ce7e5d0
Sam <sbrobson@crimson.ua.edu>
2025-10-14 12:44:48 -0500

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<div class="h-screen flex flex-col">
+	<header class="flex h-15 w-full items-center bg-black px-3 border-zinc-600 border-b">
+		<img src="/tcflogo.png" class="h-10" alt="Terence Crutcher Foundation logo" />
+		<h1 class="text-white pl-3 text-2xl font-title">TULSA ANIMAL BITES</h1>
+		<p class="ml-auto text-gray-400 text-sm">Last Updated: October 13, 2025</p>
+	</header>
+	<iframe class="w-full grow" src="/bites.html" title="Kepler"></iframe>
+</div>
